0caaf0dacb42a86ec11a24465d2556903df87541
aksakannikanti <aksakannikanti@gmail.com>
2026-06-27 00:07:39 +0530

    Added week_2

---
+++ week_2/SpringCore_and_maven/Basic_spring/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,14 @@
+package com.library.service;
+import com.library.repository.BookRepository;
+public class BookService {
+
+    private BookRepository bookRepository;
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void displayBooks() {
+        System.out.println("Book Service: Displaying books...");
+        bookRepository.getBooks();
+    }
+}
